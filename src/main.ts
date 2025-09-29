4e4156c1abc280606f19c2e936ec0c576f6a56dc
Ken <kenediidali@gmail.com>
2025-09-29 10:20:04 +0200

    WIP fix firebase api

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,23 @@ import './style.scss'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faArrowUp, faCircleChevronDown} from "@fortawesome/free-solid-svg-icons";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from "firebase/app";
 import index from "./router";
 
 library.add(faArrowUp,faCircleChevronDown)
 const firebaseConfig = {
-    authDomain: "algonova-2837d.firebaseapp.com",
-    projectId: "algonova-2837d",
-    storageBucket: "algonova-2837d.firebasestorage.app",
-    messagingSenderId: "386684462943",
-    appId: "1:386684462943:web:de3429bf4dbc3f8017f8ea",
-    measurementId: "G-K0TW15BJHD"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
